084c9d0d0b8c7512f7373a56d708e6224c5966c3
Guts <wtossici@gmail.com>
2026-08-10 17:49:59 +0200

    added adam_epsilon 1e-6 for DeBERTa

--- src/train.py
+++ src/train.py
@@ -66,7 +66,8 @@ def main():
     # Iperparametri definiti nella guida
     training_args = TrainingArguments(
         output_dir=args.output_dir,
-        learning_rate=1e-5 if "deberta" in args.model else 2e-5,
+        learning_rate=2e-5,
+        adam_epsilon=1e-6,
         num_train_epochs=1 if args.smoke_test else 2,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=32,
